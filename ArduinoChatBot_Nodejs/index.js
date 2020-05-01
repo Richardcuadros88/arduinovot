1f756c858aae6c2492ba8184bc60e517d7f9ead7
unknown <richardct8@gmail.com>
2020-05-01 16:38:43 -0500

    se corrigio el puerto com

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -6,7 +6,7 @@ const bot = new TelegramBot(token, {polling: true});
 
 var IdMiChat =1189408516;
 var SerialPort  = require ( 'serialport' );
-var Mipuerto= new SerialPort ( '/dev/COM3', {
+var Mipuerto= new SerialPort ( 'COM3', {
   baudRate: 9600,
   autoOpen: true
 } ) ;
