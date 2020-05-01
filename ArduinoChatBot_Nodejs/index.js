2f84f72916db4e51324b997d037b128cf004016e
unknown <richardct8@gmail.com>
2020-04-30 20:45:25 -0500

    modificado para los mensajes con arduino

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -4,6 +4,12 @@ const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
 
-  const chatId = msg.chat.id;
+bot.on('message', (msg) => {
+ const chatId = msg.chat.id;
+ var Mensaje = msg.text;
+ if (mesaje == "encender" {
+   console.log ("apagar el led");
+ }
+ bot.sendMessage(chatId, 'Recivi tu mensaje');
+});
