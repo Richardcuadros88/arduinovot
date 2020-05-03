035ce77bb696776abc53ecdafc5f5d335a1f5c1c
unknown <richardct8@gmail.com>
2020-05-03 17:38:34 -0500

    codigo original

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -2,38 +2,38 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {
+  polling: true
+});
+var IdMiChat = 000;//cambiar por tu ID del chat
 
-var IdMiChat =1189408516;
-var SerialPort  = require ( 'serialport' );
-var Mipuerto= new SerialPort ( 'COM3', {
+var SerialPort = require('serialport');
+var MiPuerto = new SerialPort('/dev/ttyUSB0', {
   baudRate: 9600,
   autoOpen: true
-} ) ;
-
-
-bot.on('message', (msg) => {
- const chatId = msg.chat.id;
- console.log("El ID del char"+ chatId);
- var Mensaje = msg.text;
- if (Mensaje == "Encender") {
-   console.log ("Encendiendo el led");
-    bot.sendMessage(chatId, 'Encendere el led');
-    Mipuerto.write(H);
- } else if (Mensaje == "Apagar"){
-   console.log ("Apagar el led");
-    bot.sendMessage(chatId, 'Apagare el led');
-    Mipuerto.write(L);
-
- }
 });
 
-Mipuerto.setEncoding('utf8');
-
-Mipuerto.on('data', function(data){
-  console.log ("lo que entro es"+ data);
-  if (data [0]=='H') {
-    console.log("Boton Precionado");
-    bot.sendMessage(IdMiChat, "se preciono el boton ");
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  console.log("El ID del char" + chatId);
+  var Mensaje = msg.text;
+  if (Mensaje == "Encender") {
+    console.log("encendiendo el led");
+    bot.sendMessage(chatId, 'Encendere el led');
+    MiPuerto.write("H");
+  } else if (Mensaje == "Apagar") {
+    console.log("apagar el led");
+    bot.sendMessage(chatId, 'Apagare el led');
+    MiPuerto.write("L");
   }
- });
+});
+
+MiPuerto.setEncoding('utf8');
+
+MiPuerto.on('data', function(data) {
+  console.log("Lo que entro es " + data);
+  if (data[0] == 'H') {
+    console.log("Boton Precionado");
+    bot.sendMessage(IdMiChat, "Se preciono el boton");
+  }
+});
