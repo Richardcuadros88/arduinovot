53b3442c92010dbf04b82c515cf1bd5ce78fce13
unknown <richardct8@gmail.com>
2020-05-03 18:05:49 -0500

    codigo original funciona; el switch es un sensor, manda un mensaje al celular y a travez de telegram encendemos y apagamos un alarma ( ejemplo )

--- ArduinoChatBot_Arduino/ArduinoChatBot_Arduino.ino
+++ ArduinoChatBot_Arduino/ArduinoChatBot_Arduino.ino
@@ -1,33 +1,14 @@
-
-int estado = 0;
-int estadoanterior =0;
-int salida= 0;
+int Boton = 2;
+int Led = 9;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);     // Boton (entrada del pin 2) 
-  pinMode(9, OUTPUT);             // LED (salida del pin 9)
+  pinMode(Boton, INPUT_PULLUP);
+  pinMode(Led, OUTPUT);
 }
 
 void loop() {
-  estado = digitalRead(2);      // leer el estdo del boton
-  if ((estado== LOW)&&(estadoanterior==HIGH)){               // etado del boton bajo, inicia enn 0
-    salida =1 - salida;
-    delay (500);
-    
-    }
-    estadoanterior = estado;
-    if (salida==1){
-    digitalWrite(9, HIGH);          // encendemos el LED       
-     }
-
-    else  {
-      digitalWrite(9, LOW);
-
-// programacion monitor arduino
-
-}
-  if (digitalRead(2) == 0) {
+  if (digitalRead(Boton) == 0) {
     Serial.println("H");
     delay(500);
   }
@@ -35,10 +16,11 @@ void loop() {
   if (Serial.available()) {
     char Letra = Serial.read();
     if (Letra == 'H') {
-      digitalWrite(9, HIGH);
+      digitalWrite(Led, HIGH);
     }
     else if (Letra == 'L') {
-      digitalWrite(9, LOW);
+      digitalWrite(Led, LOW);
     }
   }
+
 }
