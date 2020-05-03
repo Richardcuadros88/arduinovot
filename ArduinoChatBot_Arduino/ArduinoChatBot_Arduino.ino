1b93d4b5903a092b781dfbfc1dc0e3d8f21d4b75
unknown <richardct8@gmail.com>
2020-05-03 17:22:32 -0500

    codigo original

--- ArduinoChatBot_Arduino/ArduinoChatBot_Arduino.ino
+++ ArduinoChatBot_Arduino/ArduinoChatBot_Arduino.ino
@@ -1,45 +1,26 @@
-
-
-int estado = 0;
-int estadoanterior =0;
-int salida= 0;
+int Boton = 2;
+int Led = 9;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);     // Boton (entrada del pin 2) 
-  pinMode(9, OUTPUT);             // LED (salida del pin 9)
+  pinMode(Boton, INPUT_PULLUP);
+  pinMode(Led, OUTPUT);
 }
 
 void loop() {
-  estado = digitalRead(2);      // leer el estdo del boton
-  if ((estado== LOW)&&(estadoanterior==HIGH)){               // etado del boton bajo, inicia enn 0
-    salida =1 - salida;
-    delay (500);
-    
-    }
-    estadoanterior = estado;
-    if (salida==1){
-    digitalWrite(9, HIGH);          // encendemos el LED       
-     }
-
-    else  {
-      digitalWrite(9, LOW);
-
-// programacion monitor arduino
-
-}
-  if (digitalRead(2) == 0) {
+  if (digitalRead(Boton) == 0) {
     Serial.println("H");
-    delay(200);
+    delay(500);
   }
 
   if (Serial.available()) {
     char Letra = Serial.read();
     if (Letra == 'H') {
-      digitalWrite(9, HIGH);
+      digitalWrite(Led, HIGH);
     }
     else if (Letra == 'L') {
-      digitalWrite(9, LOW);
+      digitalWrite(Led, LOW);
     }
   }
+
 }
